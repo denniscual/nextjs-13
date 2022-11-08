1a399ff835e91cf534413fd89c5255fc6904d0a2
Dennis Cual <denniscual0716@gmail.com>
2022-11-09 02:45:30 +0800

    disable prefetching at root path

--- app/nav-bar.tsx
+++ app/nav-bar.tsx
@@ -4,10 +4,18 @@ import styles from "./nav-bar.module.css";
 export default function NavBar() {
   return (
     <nav className={styles.nav}>
-      <NavLink href="/">Home</NavLink>
-      <NavLink href="/users">Users</NavLink>
-      <NavLink href="/products">Products</NavLink>
-      <NavLink href="/admin">Admin</NavLink>
+      <NavLink href="/" prefetch={false}>
+        Home
+      </NavLink>
+      <NavLink href="/users" prefetch={false}>
+        Users
+      </NavLink>
+      <NavLink href="/products" prefetch={false}>
+        Products
+      </NavLink>
+      <NavLink href="/admin" prefetch={false}>
+        Admin
+      </NavLink>
     </nav>
   );
 }
